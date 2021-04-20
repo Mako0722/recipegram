1712c90b0644aa5c28d66e46cab1e1e48f453d81
Mako0530 <zhongtiancheng79@gmail.com>
2021-04-20 20:56:16 +0900

    レシピの投稿機能

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,30 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    # 誰が投稿しているかハッキリさせる
+    @recipe.user_id = current_user.id
+    # 保存する
+    @recipe.save 
+    redirect_to recipe_path(@recipe)
+  end
+
+  def edit
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :image)
+  end
+
+
+end
